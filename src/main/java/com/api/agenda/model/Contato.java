b06cc3af8ea78a308924acaac77366a3be3169ca
aldenor-neto <aldenor05062010@gmail.com>
2024-02-22 18:37:14 -0300

    validando campos

--- src/main/java/com/api/agenda/model/Contato.java
+++ src/main/java/com/api/agenda/model/Contato.java
@@ -5,6 +5,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,8 +24,13 @@ public class Contato {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
     private String nome;
+@NotBlank
+@Email
     private String email;
+@NotBlank
     private String telefone;
 
 }
